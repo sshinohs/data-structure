f3328845dec3f015e7eafadef628f15c1686956c
sshinohs <sshinohs@gmail.com>
2022-03-21 15:27:41 +0900

    ExpressionTree 적용을 위해 Stack 수정

--- 08_tree/ExpressionTree/ListBaseStack.h
+++ 08_tree/ExpressionTree/ListBaseStack.h
@@ -1,10 +1,12 @@
 #ifndef __LB_STACK_H__
 #define __LB_STACK_H__
 
+#include "BinaryTree2.h"
+
 #define TRUE        1
 #define FALSE       0
 
-typedef int Data;
+typedef BTreeNode * Data;
 
 typedef struct _node
 {
